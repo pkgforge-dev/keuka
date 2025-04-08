7e91e70cd2e7dbc4e202af714ccfca9b8a48df94
Nickolas Burr <nickolasburr@gmail.com>
2025-04-07 23:22:36 -0400

    Refactor mem.c

--- src/mem.c
+++ src/mem.c
@@ -10,57 +10,87 @@ const Except_T Mem_Failed = {
 	"Allocation Failed"
 };
 
-void *Mem_alloc (long nbytes, const char *file, int line) {
-	void *ptr;
+void *Mem_alloc(
+	long nbytes,
+	const char *file,
+	int line
+) {
+	void *ptr = NULL;
 
 	assert(nbytes > 0);
 	ptr = malloc(nbytes);
 
-	if (is_null(ptr)) {
-		if (is_null(file)) {
+	if (ptr == NULL) {
+		if (file == NULL) {
 			RAISE(Mem_Failed);
 		} else {
-			Except_raise(&Mem_Failed, file, line);
+			Except_raise(
+				&Mem_Failed,
+				file,
+				line
+			);
 		}
 	}
 
 	return ptr;
 }
 
-void *Mem_calloc (long count, long nbytes, const char *file, int line) {
-	void *ptr;
+void *Mem_calloc(
+	long count,
+	long nbytes,
+	const char *file,
+	int line
+) {
+	void *ptr = NULL;
 
 	assert(count > 0);
 	assert(nbytes > 0);
 	ptr = calloc(count, nbytes);
 
-	if (is_null(ptr)) {
-		if (is_null(file)) {
+	if (ptr == NULL) {
+		if (file == NULL) {
 			RAISE(Mem_Failed);
 		} else {
-			Except_raise(&Mem_Failed, file, line);
+			Except_raise(
+				&Mem_Failed,
+				file,
+				line
+			);
 		}
 	}
 
 	return ptr;
 }
 
-void Mem_free (void *ptr, const char *file, int line) {
+void Mem_free(
+	void *ptr,
+	const char *file,
+	int line
+) {
 	if (ptr) {
 		free(ptr);
 	}
 }
 
-void *Mem_resize (void *ptr, long nbytes, const char *file, int line) {
+void *Mem_resize(
+	void *ptr,
+	long nbytes,
+	const char *file,
+	int line
+) {
 	assert(ptr);
 	assert(nbytes > 0);
 	ptr = realloc(ptr, nbytes);
 
-	if (is_null(ptr)) {
-		if (is_null(file)) {
+	if (ptr == NULL) {
+		if (file == NULL) {
 			RAISE(Mem_Failed);
 		} else {
-			Except_raise(&Mem_Failed, file, line);
+			Except_raise(
+				&Mem_Failed,
+				file,
+				line
+			);
 		}
 	}
 
